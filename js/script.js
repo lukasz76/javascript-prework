56f756130ce08270bc04d5137c45b9c1dbf2fe70
Lukasz Kaminski <lukaszmoth@gmail.com>
2020-02-20 20:35:34 +0100

    Add js file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+let computerMove = 'kamień';
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier';
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamien, to przegrywasz!');
 
 
 
